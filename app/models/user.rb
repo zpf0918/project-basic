a573cb529710ef8bb837635865e6f0ee39ae1718
jonathan <jonathan@kafeidebaideMacBook-Pro.local>
2017-12-21 12:30:28 +0800

    alter development database

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,7 +3,7 @@ class User < ApplicationRecord
   validates :name, presence: true, length: { maximum: 50 }
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z\d\-]+)*\.[a-z]+\z/i
   validates :email, presence: true, length: { maximum: 255 },#邮箱地址唯一，且要在数据库层面加上索引
-                    format { with: VALID_EMAIL_REGEX }, #邮箱格式，VALID_EMAIL_REGEX为常量
+                    format: { with: VALID_EMAIL_REGEX }, #邮箱格式，VALID_EMAIL_REGEX为常量
                     uniqueness: { case_sensitive: false } #邮箱地址唯一,不区分大小写
   validates :password, presence: true, length: { maximum: 6} #密码长度
 
